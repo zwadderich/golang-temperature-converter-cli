e7e50a8a9064dbdb1d957020a6a336363c587f30
Carlos Souza <carloshrsouza@gmail.com>
2020-01-24 09:41:16 -0500

    Add initial code and failing tests

---
+++ README.md
@@ -0,0 +1,12 @@
+# Temperature Converter CLI
+
+Converts temperature between Fahrenheit and Celsius.
+
+## Usage
+
+Compile with `go build -o converter`.
+
+Then, invoke the binary passing as argument the unit of temperature we want to convert **from**.
+For example:
+
+`./converter C` to convert from Celsius to Fahrenheit or `./converter F` to convert from Fahrenheit to Celsius.
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+var originUnit string
+var originValue float64
+
+var shouldConvertAgain string
+
+var err error
+
+var errInvalidArguments = errors.New("Invalid arguments")
+var errReadingInput = errors.New("Error reading input")
+
+func main() {
+
+	for {
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+
+		if shouldConvertAgain != "Y" {
+			fmt.Println("Good bye!")
+			break
+		}
+	}
+}
+
+func printError(err error) {
+	fmt.Fprintf(os.Stderr, "error: %v\n", err)
+	os.Exit(1)
+}
+
+func convertToCelsius(value float64) {
+	convertedValue := (value - 32) * 5 / 9
+	fmt.Printf("%v F = %.0f C\n", value, convertedValue)
+}
+
+func convertToFahrenheit(value float64) {
+	convertedValue := (value * 9 / 5) + 32
+	fmt.Printf("%v C = %.0f F\n", value, convertedValue)
+}
